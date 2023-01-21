b1035a8a4e779157b336e9586efac218291b17e8
Dilafruz7777777 <dilafruzasamutdinovaflutter@gmail.com>
2023-01-21 18:39:57 +0500

    get information from Google account

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-
-
-
-
-
-
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
-
-
-// firebases
-
-
-
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,6 +16,73 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(onPressed: () async {
+             try{
+               GoogleSignIn _googleSingIn=GoogleSignIn(
+                 // scopes: [
+                 //   'email',
+                 //   'https://www.googleapis.com/auth/contacts.readonly',
+                 // ]
+               );
+               var data=await _googleSingIn.signIn();
+               print(data?.id);
+               print(data?.email);
+               print(data?.photoUrl);
+               print(data?.displayName);
+               _googleSingIn.signOut();
+             }catch(e){
+               print(e);
+             }
+
+            }, child: const Text('Google sign'))
+          ],
+        ),
+      ),
     );
   }
 }
